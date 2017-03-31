660f2a14fa439d219f929037a9d0e749dcf9eda2
gahan-quixom <gahan@quixom.com>
2017-03-31 14:54:18 +0530

    urls for class based view added

--- details/urls.py
+++ details/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     url(r'^product/(?P<book_id>[0-9]+)/$', views.product_page, name='product_page'),
     url(r'^publisher/(?P<publisher_id>[0-9]+)/$', views.publisher_page, name='publisher_page'),
     url(r'^author/(?P<author_id>[0-9]+)/$', views.author_page, name='author_page'),
-    url(r'^delete_entry/', views.delete_entry),
-    url(r'^stock_availability', views.stock_availability, name='stock_change'),
-    url(r'^book_create', views.BookCreate.as_view(), name='add_book')
+    url(r'^delete-entry/(?P<pk>\d+)/$', views.BookDeleteView.as_view(), name='delete_book'),
+    url(r'^stock-availability', views.stock_availability, name='stock_change'),
+    url(r'^book-create', views.BookCreate.as_view(), name='add_book'),
+    url(r'^book-edit/(?P<pk>\d+)/$', views.BookEditView.as_view(), name='edit_book')
 ]
