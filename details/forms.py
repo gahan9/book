a2d22a8144743c8c5e51d3f9649c8bb303529cc7
gahan-quixom <gahan@quixom.com>
2017-04-01 18:37:51 +0530

    Updated ChangePassword form

--- details/forms.py
+++ details/forms.py
@@ -3,8 +3,7 @@
 from __future__ import unicode_literals
 
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, \
-    PasswordResetForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordResetForm, PasswordChangeForm
 from django.contrib.auth import get_user_model
 from django.contrib.auth import password_validation
 
@@ -12,7 +11,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Div, Field, HTML, Submit
 from crispy_forms.bootstrap import PrependedText, FormActions, StrictButton
 
-from book.helpers import add_book_helper, search_helper
+from book.helpers import *
 from .models import *
 
 
@@ -61,52 +60,23 @@ class SignUpForm(UserCreationForm, PasswordResetForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 
-class ChangePassword(forms.Form):
-    old_password = forms.CharField(
-        label="Old password",
-        strip=False,
-        widget=forms.PasswordInput(
-            attrs={'name': 'old_password'}),
-    )
+class ChangePassword(PasswordChangeForm):
+    old_password = forms.CharField(label="Current Password", widget=forms.PasswordInput(attrs={'name': 'old_password'}),)
     password1 = forms.CharField(
-        label="Password",
-        max_length=30, strip=False,
+        label="Password", max_length=30, strip=False,
         widget=forms.TextInput(
             attrs={'class': 'form-control',
                    'name': 'new password',
                    'type': 'password'}))
-    password2 = forms.CharField(
-        label="Password confirmation",
-        strip=False,
-        max_length=30,
-        widget=forms.TextInput(
-            attrs={'class': 'form-control',
-                   'name': 'new password confirmation',
-                   'type': 'password'}))
+    password2 = forms.CharField(label="Password confirmation", strip=False, max_length=30,
+                                widget=forms.TextInput(attrs={'class': 'form-control',
+                                                              'name': 'new password confirmation',
+                                                              'type': 'password'}))
 
     def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user', None)
         super(ChangePassword, self).__init__(*args, **kwargs)
-
-    def clean_password2(self):
-        print("data")
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("The two password fields didn't match.")
-        # self.username = self.cleaned_data.get('username')
-        password_validation.validate_password(self.cleaned_data.get('password2'))
-        return password2
-
-    def clean_old_password(self):
-        """
-        Validates that the old_password field is correct.
-        """
-        print("sell")
-        old_password = self.cleaned_data["old_password"]
-        if not self.user.check_password(old_password):
-            raise forms.ValidationError('Incorrect old password')
-        return old_password
+        self.helper = password_change_helper
+        # self.fields['password1'].label = ""
 
     class Meta:
         model = get_user_model()
